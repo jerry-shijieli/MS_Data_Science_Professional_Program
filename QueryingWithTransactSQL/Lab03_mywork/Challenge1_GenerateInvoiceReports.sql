18560340bd9dd288ca6ff1ec61d21bf408fd2baa
Jerry Shijie Li <sli41@ncsu.edu>
2016-11-12 21:22:06 -0500

    lab #3 is done!

---
+++ QueryingWithTransactSQL/Lab03_mywork/Challenge1_GenerateInvoiceReports.sql
@@ -0,0 +1,16 @@
+﻿-- Retrieve customer orders
+select cst.CompanyName, soh.SalesOrderID, soh.TotalDue
+from SalesLT.Customer as cst
+join SalesLT.SalesOrderHeader as soh
+on cst.CustomerID = soh.CustomerID;
+
+-- Retrieve customer orders with addresses
+select cst.CompanyName, soh.SalesOrderID, soh.TotalDue, 
+	addr.AddressLine1+' '+ISNULL(addr.AddressLine2+' ', '')+addr.City+' '+addr.StateProvince+' '+addr.PostalCode+' '+addr.CountryRegion as MainOfficAddress
+from SalesLT.Customer as cst
+join SalesLT.SalesOrderHeader as soh
+on cst.CustomerID = soh.CustomerID
+join SalesLT.CustomerAddress as caddr
+on cst.CustomerID = caddr.CustomerID and caddr.AddressType = 'Main Office'
+join SalesLT.Address as addr
+on addr.AddressID = caddr.AddressID;
---
+++ QueryingWithTransactSQL/Lab03_mywork/Challenge2_RetrieveSalesData.sql
@@ -0,0 +1,25 @@
+﻿-- Retrieve a list of all customers and their orders
+select c.CompanyName, c.FirstName+' '+c.LastName as Contacts, soh.SalesOrderID, soh.TotalDue
+from SalesLT.Customer as c
+full join SalesLT.SalesOrderHeader as soh
+on c.CustomerID = soh.CustomerID
+order by soh.SalesOrderID desc, soh.TotalDue desc;
+
+-- Retrieve a list of customers with no address
+select c.CustomerID, c.CompanyName, c.FirstName+' '+c.LastName as Contacts, c.Phone
+from SalesLT.Customer as c
+left join SalesLT.CustomerAddress as ca
+on c.CustomerID = ca.CustomerID
+where ca.AddressID IS NULL;
+
+-- Retrieve a list of customers and products without orders
+select c.CustomerID, p.ProductID
+from SalesLT.Customer as c
+full join SalesLT.SalesOrderHeader as soh
+on c.CustomerID = soh.CustomerID
+full join SalesLT.SalesOrderDetail as sod
+on soh.SalesOrderID = sod.SalesOrderID
+full join SalesLT.Product as p
+on sod.ProductID = p.ProductID
+where sod.SalesOrderID IS NULL
+order by ProductID, CustomerID;
