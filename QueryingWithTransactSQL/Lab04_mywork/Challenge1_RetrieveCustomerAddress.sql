5959b69886ce94dab41bddba413c93c19015b30f
Jerry Shijie Li <sli41@ncsu.edu>
2016-11-13 11:13:46 -0500

    lab #4 is done!

---
+++ QueryingWithTransactSQL/Lab04_mywork/Challenge1_RetrieveCustomerAddress.sql
@@ -0,0 +1,35 @@
+﻿-- Retrieve billing addresses
+select c.CompanyName, addr.AddressLine1, addr.City, 'Billing' as AddressType
+from SalesLT.Customer as c
+join SalesLT.CustomerAddress as ca
+on c.CustomerID = ca.CustomerID
+join SalesLT.Address as addr
+on ca.AddressID = addr.AddressID
+where ca.AddressType = 'Main Office';
+
+-- Retrieve shipping addresses
+select c.CompanyName, addr.AddressLine1, addr.City, 'Shipping' as AddressType
+from SalesLT.Customer as c
+join SalesLT.CustomerAddress as ca
+on c.CustomerID = ca.CustomerID
+join SalesLT.Address as addr
+on ca.AddressID = addr.AddressID
+where ca.AddressType = 'Shipping';
+
+-- Combine billing and shippping addresses
+select c.CompanyName, addr.AddressLine1, addr.City, 'Billing' as AddressType
+from SalesLT.Customer as c
+join SalesLT.CustomerAddress as ca
+on c.CustomerID = ca.CustomerID
+join SalesLT.Address as addr
+on ca.AddressID = addr.AddressID
+where ca.AddressType = 'Main Office'
+UNION ALL
+select c.CompanyName, addr.AddressLine1, addr.City, 'Shipping' as AddressType 
+from SalesLT.Customer as c
+join SalesLT.CustomerAddress as ca
+on c.CustomerID = ca.CustomerID
+join SalesLT.Address as addr
+on ca.AddressID = addr.AddressID
+where ca.AddressType = 'Shipping'
+order by c.CompanyName, AddressType;
---
+++ QueryingWithTransactSQL/Lab04_mywork/Challenge2_FilterCustomerAddresses.sql
@@ -0,0 +1,35 @@
+﻿-- Retrieve customes with only a main office address
+select c.CompanyName
+from SalesLT.Customer as c
+join SalesLT.CustomerAddress as ca
+on c.CustomerID = ca.CustomerID
+join SalesLT.Address as addr
+on ca.AddressID = addr.AddressID
+where ca.AddressType = 'Main Office'
+except
+select c.CompanyName
+from SalesLT.Customer as c
+join SalesLT.CustomerAddress as ca
+on c.CustomerID = ca.CustomerID
+join SalesLT.Address as addr
+on ca.AddressID = addr.AddressID
+where ca.AddressType = 'Shipping'
+order by c.CompanyName;
+
+-- Retrieve only customers with both a main office address and a shipping address
+select c.CompanyName
+from SalesLT.Customer as c
+join SalesLT.CustomerAddress as ca
+on c.CustomerID = ca.CustomerID
+join SalesLT.Address as addr
+on ca.AddressID = addr.AddressID
+where ca.AddressType = 'Main Office'
+intersect
+select c.CompanyName
+from SalesLT.Customer as c
+join SalesLT.CustomerAddress as ca
+on c.CustomerID = ca.CustomerID
+join SalesLT.Address as addr
+on ca.AddressID = addr.AddressID
+where ca.AddressType = 'Shipping'
+order by c.CompanyName;
