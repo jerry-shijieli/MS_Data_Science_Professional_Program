1fc30b79b47a60b0b1f0423ee081b83d8737c46e
Jerry Shijie Li <sli41@ncsu.edu>
2016-11-13 21:19:36 -0500

    lab #6 is done!

---
+++ QueryingWithTransactSQL/Lab06_mywork/Challenge1_RetrieveProductPriceInformation.sql
@@ -0,0 +1,40 @@
+﻿-- Retrieve products whose list price is higher than the average unit price
+select p.ProductID, p.Name, p.ListPrice
+from SalesLT.Product as p
+where p.ListPrice > (
+	select AVG(sod.UnitPrice)
+	from SalesLT.SalesOrderDetail as sod
+);
+
+-- Retrieve products with a list price of $100 or more that have been sold for less than $100
+select p.ProductID, p.Name, p.ListPrice
+from SalesLT.Product as p
+where p.ProductID in (
+	select sod.ProductID 
+	from SalesLT.SalesOrderDetail as sod
+	where sod.UnitPrice < 100
+)
+and p.ListPrice >= 100;
+
+
+-- Retrieve the cost, list price and average sellign price for each product
+select p.ProductID, p.Name, p.StandardCost, p.ListPrice,
+	(select AVG(sod.UnitPrice)
+	 from SalesLT.SalesOrderDetail as sod
+	 where p.ProductID = sod.ProductID
+	) as AverageUnitPrice
+from SalesLT.Product as p
+order by p.ProductID;
+
+-- Retrieve products that have an average selling price that is lower than the cost
+select p.ProductID, p.Name, p.StandardCost, p.ListPrice,
+	(select AVG(sod.UnitPrice)
+	 from SalesLT.SalesOrderDetail as sod
+	 where p.ProductID = sod.ProductID
+	) as AverageUnitPrice
+from SalesLT.Product as p
+where p.StandardCost > 
+	(select AVG(sod.UnitPrice)
+	 from SalesLT.SalesOrderDetail as sod
+	 where p.ProductID = sod.ProductID)
+order by p.ProductID;
---
+++ QueryingWithTransactSQL/Lab06_mywork/Challenge2_RetrieveCustomerinformation.sql
@@ -0,0 +1,13 @@
+﻿-- Retrieve customer information for all sales orders
+select soh.SalesOrderID, GCI.CustomerID, GCI.FirstName, GCI.LastName, soh.TotalDue
+from SalesLT.SalesOrderHeader as soh
+cross apply dbo.ufnGetCustomerInformation(soh.CustomerID) as GCI
+order by soh.SalesOrderID;
+
+-- Retrieve customer address information 
+select ca.CustomerID, GCI.FirstName, GCI.LastName, addr.AddressLine1, addr.City
+from SalesLT.CustomerAddress as ca
+inner join SalesLT.Address as addr
+on ca.AddressID = addr.AddressID
+cross apply dbo.ufnGetCustomerInformation(ca.CustomerID) as GCI
+order by ca.CustomerID;
