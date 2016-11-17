10a018bfd9e1fa9faf109302d2c486184b38ca62
Jerry Shijie Li <sli41@ncsu.edu>
2016-11-17 13:19:42 -0500

    lab#7 is done! Upload lab #8 material

---
+++ QueryingWithTransactSQL/Lab07_mywork/Challenge1_RetrieveProductInformation.sql
@@ -0,0 +1,25 @@
+﻿-- Retrieve product model descriptions
+select p.ProductID, p.Name, pmcd.Summary
+from SalesLT.Product as p
+inner join SalesLT.vProductModelCatalogDescription as pmcd
+on p.ProductModelID = pmcd.ProductModelID
+order by p.ProductID;
+
+-- Create a table of distinct colors
+declare @Colors as table (Color nvarchar(30));
+
+insert into @Colors
+select distinct Color from SalesLT.Product;
+
+select ProductID, Name, Color
+from SalesLT.Product
+where Color in (select Color from @Colors);
+
+-- Retrieve product parent categories
+select gac.ParentProductCategoryName as ParentCategory,
+	gac.ProductCategoryName as Category,
+	p.ProductID, p.Name as ProductName
+from SalesLT.Product as p
+inner join dbo.ufnGetAllCategories() as gac
+on p.ProductCategoryID = gac.ProductCategoryID
+order by ParentCategory, Category, ProductName;
---
+++ QueryingWithTransactSQL/Lab07_mywork/Challenge2_RetrieveCustomerSalesRevenue.sql
@@ -0,0 +1,22 @@
+﻿-- Retrieve sales revenue by customer and contact
+-- using derived table
+select CompanyContactName, sum(TotalSales) as TotalRevenue
+from
+	(select c.CompanyName+' ('+c.FirstName+' '+c.LastName+')', soh.TotalDue
+	from SalesLT.SalesOrderHeader as soh
+	inner join SalesLT.Customer as c
+	on soh.CustomerID = c.CustomerID) as derived_SalesOrder(CompanyContactName, TotalSales)
+group by CompanyContactName
+order by CompanyContactName;
+
+-- using CTE
+with CompanySalesOrder(CompanyContactName, TotalSales)
+as 
+(select concat(c.CompanyName, concat(' ('+c.FirstName, ' '+c.LastName+')')), soh.TotalDue
+from SalesLT.SalesOrderHeader as soh
+inner join SalesLT.Customer as c
+on soh.CustomerID = c.CustomerID)
+select CompanyContactName, sum(TotalSales)
+from CompanySalesOrder
+group by CompanyContactName
+order by CompanyContactName;
