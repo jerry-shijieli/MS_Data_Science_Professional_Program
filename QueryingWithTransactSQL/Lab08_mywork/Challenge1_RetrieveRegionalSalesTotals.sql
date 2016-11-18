0c8a1efa55db4e203e66801d1357e06109bfd258
Jerry Shijie Li <sli41@ncsu.edu>
2016-11-17 22:15:25 -0500

    lab #8 is done!

---
+++ QueryingWithTransactSQL/Lab08_mywork/Challenge1_RetrieveRegionalSalesTotals.sql
@@ -0,0 +1,32 @@
+﻿-- Retrieve totals for country/region and state/province
+SELECT a.CountryRegion, a.StateProvince, SUM(soh.TotalDue) AS Revenue
+FROM SalesLT.Address AS a
+JOIN SalesLT.CustomerAddress AS ca ON a.AddressID = ca.AddressID
+JOIN SalesLT.Customer AS c ON ca.CustomerID = c.CustomerID
+JOIN SalesLT.SalesOrderHeader as soh ON c.CustomerID = soh.CustomerID
+GROUP BY ROLLUP(a.CountryRegion, a.StateProvince)
+ORDER BY a.CountryRegion, a.StateProvince;
+
+-- Indicate the grouping level in the results
+SELECT a.CountryRegion, a.StateProvince,
+	iif(GROUPING_ID(a.CountryRegion)=1 and GROUPING_ID(a.StateProvince)=1, 'Total', 
+	iif(grouping_id(a.StateProvince)=1, a.CountryRegion+'Subtotal', a.StateProvince+'Subtotal')) as Level,
+	SUM(soh.TotalDue) AS Revenue
+FROM SalesLT.Address AS a
+JOIN SalesLT.CustomerAddress AS ca ON a.AddressID = ca.AddressID
+JOIN SalesLT.Customer AS c ON ca.CustomerID = c.CustomerID
+JOIN SalesLT.SalesOrderHeader as soh ON c.CustomerID = soh.CustomerID
+GROUP BY ROLLUP(a.CountryRegion, a.StateProvince)
+ORDER BY a.CountryRegion, a.StateProvince;
+
+-- Add a grouping level for cities
+SELECT a.CountryRegion, a.StateProvince, a.City,
+	choose(1+GROUPING_ID(a.CountryRegion)+GROUPING_ID(a.StateProvince)+GROUPING_ID(a.City), 
+	a.City+' Subtotal', a.StateProvince+' Subtotal', a.CountryRegion+' Subtotal', 'Total') as Level,
+	SUM(soh.TotalDue) AS Revenue
+FROM SalesLT.Address AS a
+JOIN SalesLT.CustomerAddress AS ca ON a.AddressID = ca.AddressID
+JOIN SalesLT.Customer AS c ON ca.CustomerID = c.CustomerID
+JOIN SalesLT.SalesOrderHeader as soh ON c.CustomerID = soh.CustomerID
+GROUP BY ROLLUP(a.CountryRegion, a.StateProvince, a.City)
+ORDER BY a.CountryRegion, a.StateProvince, a.City;
---
+++ QueryingWithTransactSQL/Lab08_mywork/Challenge2_RetrieveCustomerSalesRevenueByCategory.sql
@@ -0,0 +1,11 @@
+﻿-- Retrieve customer sales revenue for each parent category
+select * from
+(select c.CompanyName, gac.ParentProductCategoryName, sod.LineTotal
+from SalesLT.SalesOrderDetail as sod
+join SalesLT.SalesOrderHeader as soh on sod.SalesOrderID=soh.SalesOrderID
+join SalesLT.Customer as c on c.CustomerID=soh.CustomerID
+join SalesLT.Product as p on p.ProductID=sod.ProductID
+join SalesLT.vGetAllCategories as gac on gac.ProductCategoryID=p.ProductCategoryID) as source_table
+pivot
+(sum(LineTotal) for ParentProductCategoryName in ([Accessories],[Bikes],[Clothing],[Components])) as pvt
+order by CompanyName;
