14b4b9d19dd78f583e50e497177e862f1bc169e3
Jerry Shijie Li <sli41@ncsu.edu>
2016-11-18 16:54:11 -0500

    lab #11 is done!

---
+++ QueryingWithTransactSQL/Lab11_mywork/Challenge1_LoggingErrors.sql
@@ -0,0 +1,20 @@
+﻿-- Throw an error for non-existent orders
+-- Handle errors
+declare @SalesOrderID int = 0;
+
+begin try
+	if exists (select * from SalesLT.SalesOrderHeader where SalesOrderID = @SalesOrderID)
+	begin
+		delete from SalesLT.SalesOrderDetail where SalesOrderID = @SalesOrderID;
+		delete from SalesLT.SalesOrderHeader where SalesOrderID = @SalesOrderID;
+	end
+	else
+	begin
+		declare @error varchar(100);
+		set @error = 'Order #' + cast(@SalesOrderID as varchar) + ' does not exist.';
+		throw 50001, @error, 0;
+	end
+end try
+begin catch
+	print error_message(); 
+end catch
---
+++ QueryingWithTransactSQL/Lab11_mywork/Challenge2_EnsuringDataConsistency.sql
@@ -0,0 +1,31 @@
+﻿-- Implement a transaction
+declare @SalesOrderID int = 0;
+--select @SalesOrderID = MIN(SalesOrderID) from SalesLT.SalesOrderHeader; -- for test use
+
+begin try
+	if exists (select * from SalesLT.SalesOrderHeader where SalesOrderID = @SalesOrderID)
+	begin
+		begin transaction
+			delete from SalesLT.SalesOrderDetail where SalesOrderID = @SalesOrderID;
+			--throw 50001, 'Test error', 0 -- for test use
+			delete from SalesLT.SalesOrderHeader where SalesOrderID = @SalesOrderID;
+		commit transaction
+	end
+	else
+	begin
+		declare @error varchar(100);
+		set @error = 'Order #' + cast(@SalesOrderID as varchar) + ' does not exist.';
+		throw 50001, @error, 0;
+	end
+end try
+begin catch
+	if @@TRANCOUNT > 0
+	begin
+		rollback transaction;
+		throw;
+	end
+	else
+	begin
+		print error_message();
+	end 
+end catch
