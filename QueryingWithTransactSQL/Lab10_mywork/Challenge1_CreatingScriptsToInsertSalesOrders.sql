524b06d34b6c85a3c24f66dff29d9fe84b7786ed
Jerry Shijie Li <sli41@ncsu.edu>
2016-11-18 11:09:13 -0500

    lab #10 is done!

---
+++ QueryingWithTransactSQL/Lab10_mywork/Challenge1_CreatingScriptsToInsertSalesOrders.sql
@@ -0,0 +1,30 @@
+﻿-- Write code to insert an order header
+declare @OrderDate datetime = GETDATE();
+declare @DueDate datetime = DATEADD(dd, 7, GETDATE());
+declare @CustomerID int = 1;
+declare @OrderID int;
+
+set @OrderID = next value for SalesLT.SalesOrderNumber;
+
+insert into SalesLT.SalesOrderHeader (SalesOrderID, OrderDate, DueDate, CustomerID, ShipMethod)
+values (@OrderID, @OrderDate, @DueDate, @CustomerID, 'CARGO TRANSPORT 5');
+
+PRINT @OrderID;
+
+-- Write code to insert an order detail
+declare @SalesOrderID int;
+declare @ProductID int = 760;
+declare @Quantity int = 1;
+declare @UnitPrice money = 782.99;
+
+set @SalesOrderID = 0;
+
+if exists (select SalesOrderID from SalesLT.SalesOrderDetail where SalesOrderID = @SalesOrderID)
+begin
+	insert into SalesLT.SalesOrderDetail (SalesOrderID, ProductID, OrderQty, UnitPrice)
+	values (@SalesOrderID, @ProductID, @Quantity, @UnitPrice)
+end
+else
+begin 
+	print 'The order does not exist'
+end
---
+++ QueryingWithTransactSQL/Lab10_mywork/Challenge2_UpdatingBikePrices.sql
@@ -0,0 +1,39 @@
+﻿-- Write a WHILE loop to update bike prices
+declare @MarketMax money = 5000;
+declare @MarketAvg money = 2000;
+declare @TargetCategory varchar(30) = 'Bikes';
+declare @CurrentMax money;
+declare @CurrentAvg money;
+declare @PriceIncrementPercentage decimal(8,2) = 0.1;
+
+select @CurrentMax = MAX(ListPrice), @CurrentAvg = AVG(ListPrice)
+from SalesLT.Product
+where ProductCategoryID in
+	(select distinct ProductCategoryID
+	from SalesLT.vGetAllCategories
+	where ParentProductCategoryName = 'Bikes');
+
+WHILE @CurrentAvg < @MarketAvg
+begin
+	update SalesLT.Product
+	set ListPrice = ListPrice * (1+@PriceIncrementPercentage)
+	where ProductCategoryID in 
+		(select distinct ProductCategoryID
+		from SalesLT.vGetAllCategories
+		where ParentProductCategoryName = 'Bikes');
+
+	select @CurrentMax = MAX(ListPrice), @CurrentAvg = AVG(ListPrice)
+	from SalesLT.Product
+	where ProductCategoryID in
+	(select distinct ProductCategoryID
+	from SalesLT.vGetAllCategories
+	where ParentProductCategoryName = 'Bikes');
+
+	if @CurrentMax >= @MarketMax
+		break
+	else
+		continue
+end
+
+print 'New average bike price: ' + convert(varchar, @CurrentAvg);
+print 'New maximum bike price: ' + convert(varchar, @CurrentMax);
